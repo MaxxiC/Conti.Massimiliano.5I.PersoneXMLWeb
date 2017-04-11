0ba6ff4c6a30ccd94ff6578618fdffb31d4dd707
MaxxiC <andmassic@icloud.com>
2017-04-11 14:43:29 +0200

    Aggiunta File

---
+++ Conti.Massimiliano.5I.PersoneXMLWeb/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace Conti.Massimiliano._5I.PersoneXMLWeb
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            string nomeFile = HostingEnvironment.MapPath(@"~/App_Data/Persone.xml");
+            XElement data = XElement.Load(nomeFile);
+            var persone = (from l in data.Elements("Persona") select new Persona(l)).ToList();
+
+            return View(persone);
+        }
+    }
+}
---
+++ Conti.Massimiliano.5I.PersoneXMLWeb/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Conti.Massimiliano._5I.PersoneXMLWeb
+{
+    public class Persona
+    {
+        public string Nome_C { get; set; }
+        public string Cognome_C { get; set; }
+        public string Indirizzo_C { get; set; }
+        public string Number_C { get; set; }
+        public Persona(XElement e)
+        {
+            Nome_C = e.Attribute("Nome").Value;
+            Cognome_C = e.Attribute("Cognome").Value;
+            Indirizzo_C = e.Attribute("Indirizzo").Value;
+            Number_C = e.Attribute("Numero").Value;
+        }
+    }
+}
